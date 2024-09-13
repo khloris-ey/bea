42f6bb6e2c7eb1377055d078b77bcb60e34dbf74
bei-coleene <delacruzbeii290@gmail.com>
2024-09-13 20:42:48 +0800

    ndjdne

--- khloris_/khloris_ecommerce.sql
+++ khloris_/khloris_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2024 at 01:56 PM
+-- Generation Time: Sep 13, 2024 at 02:21 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -79,7 +79,27 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'flower'),
-(2, 'occasion');
+(2, 'occasion'),
+(3, 'customize');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address` varchar(80) NOT NULL,
+  `phone_num` varchar(15) NOT NULL,
+  `delivery_option` varchar(15) NOT NULL,
+  `qty` int(3) NOT NULL DEFAULT 1,
+  `total` decimal(10,2) NOT NULL,
+  `order_date` date DEFAULT curdate(),
+  `delivery` date NOT NULL,
+  `order_status` enum('Ordered','Pending','Completed') DEFAULT 'Ordered'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -126,7 +146,38 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `quantity`, `image
 (31, 'Pure Radiance', '', 9000.00, 35, 'https://res.cloudinary.com/dzvd6o0og/image/upload/v1725527225/beautiful-lilies-bouquet_3_tdc4m7.jpg', 1, 3),
 (32, 'Luminous Garden', '', 12000.00, 55, 'https://res.cloudinary.com/dzvd6o0og/image/upload/v1725527227/4665b4d2ec0b4b236dbea1ebd68bfa70_gdlzuw.jpg', 1, 3),
 (33, 'Elegant Harmony', '', 2500.00, 10, 'https://res.cloudinary.com/dzvd6o0og/image/upload/v1725527226/lily35b1-scaled_cevi14.jpg', 1, 3),
-(34, 'Opulent Splendor', '', 3500.00, 25, 'https://res.cloudinary.com/dzvd6o0og/image/upload/v1725527224/34198900b2d5482ceef4fcceadec0985_uvgewp.jpg', 1, 3);
+(34, 'Opulent Splendor', '', 3500.00, 25, 'https://res.cloudinary.com/dzvd6o0og/image/upload/v1725527224/34198900b2d5482ceef4fcceadec0985_uvgewp.jpg', 1, 3),
+(35, 'Red Rose', 'rose stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723270009/flowers/lbxdapwfesoogo1tbi8s.png', 3, 11),
+(36, 'White Rose', 'white rose stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723359130/flowers/whiterose.png', 3, 11),
+(37, 'Red Tulip', 'red tulips stem', 70.00, 60, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723359191/flowers/ms9xtemytwlhyyhqeabb.png', 3, 11),
+(38, 'Yellow Tulip', 'tulip stem', 70.00, 60, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723359199/flowers/yellowtulips.png', 3, 11),
+(39, 'White Tulip', 'tulip stem', 70.00, 60, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723359203/flowers/whitetulips.png', 3, 11),
+(40, 'Red Carnaion', 'carnation stem', 70.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723880362/flowers/redcarnation.png', 3, 11),
+(41, 'White Carnation', 'carnation stem', 70.00, 70, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723880362/flowers/stm3btzql1daz3mobly5.png', 3, 11),
+(42, 'Pink Carnation', 'carnation stem', 70.00, 60, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723880363/flowers/vsu5tp0qduzpnt6c4wjd.png', 3, 11),
+(43, 'White Lily', 'Lily stem', 60.00, 100, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725817506/flowers/emj9un5g1kt7fwhh0ogx.png', 3, 11),
+(44, 'Pink Lily', 'lily stem', 60.00, 90, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725820010/flowers/lily.png', 3, 11),
+(45, 'Orange Dahlia', 'dahlia stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725824085/flowers/rttpojbctuub3nssek3n.png', 3, 11),
+(46, 'White Dahlia', 'dahlia stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725824085/flowers/qvjekv39v4g04idwwyu8.png', 3, 11),
+(47, 'Pink Dahlia', 'dahlia stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725824086/flowers/uywfmvp2mlxuonwgdesh.png', 3, 11),
+(48, 'Violet Iris', 'iris stem', 70.00, 70, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725824294/flowers/j52tsqq0xfvt1z4kgzwe.png', 3, 11),
+(49, 'Blue Iris', 'iris stem', 70.00, 80, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725824294/flowers/fjublfznvmpfabnxkhyu.png', 3, 11),
+(50, 'Pink Hydrangea', 'hydrangea stem', 150.00, 40, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826061/flowers/imdgxzj7aakq1d4cpxso.png', 3, 11),
+(51, 'Blue Hydrangea', 'hydrangea stem', 150.00, 40, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826067/flowers/h4v3aali5jdaw1qlhokj.png', 3, 11),
+(52, 'Purple Anemone', 'anemone stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826062/flowers/y82usejj2ac2i90jldzc.png', 3, 11),
+(53, 'Red Anemone', 'anemone stem', 100.00, 50, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826753/flowers/voikv1769whr33lkm645.png', 3, 11),
+(54, 'Iris', 'iris stem', 70.00, 70, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826744/flowers/wzqu2frxrdh8e4uiu6j2.png', 3, 11),
+(55, 'Peach Peony', 'peony stem', 100.00, 60, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826062/flowers/jfmqz2y1dbifiulcvbdk.png', 3, 11),
+(56, 'Pink Peony', 'peony stem', 100.00, 65, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1725826752/flowers/bvbnyqmudbkqh0bd5zjj.png', 3, 11),
+(57, 'Sunflower', 'sunflower stem', 50.00, 100, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1726073109/flowers/ms3vruapnyhspj8hbejv.png', 3, 11),
+(58, 'Daisy', 'daisy stem', 30.00, 70, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1726072396/flowers/xaggp9utttrbdasstll8.png', 3, 11),
+(59, 'Simple Brown Wrapper', 'brown wrapper', 50.00, 100, 'https://res.cloudinary.com/dsfcl09md/image/upload/v1723359520/flowers/kjy5hmwbf14bxt0c4qox.png', 3, 9),
+(60, 'Pixie Dream', 'hydrangea bday', 12000.00, 20, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509168/images/BIRTHDAYFLOWERS/br21gmyop5xo1f3ip4mo.jpg', 2, 5),
+(61, 'Petal Poetry', 'birthday', 13000.00, 30, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509186/images/BIRTHDAYFLOWERS/rjiv8evs1l0tcafoavxf.jpg', 2, 5),
+(62, 'Crimson Kiss', 'bday', 11000.00, 20, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509196/images/BIRTHDAYFLOWERS/b3ik1hj0ujq0ongymvgs.jpg', 2, 5),
+(63, 'Blossom Granduer', 'bday', 10000.00, 30, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509294/images/BIRTHDAYFLOWERS/hyxfzseaxhtpjhn5talu.webp', 2, 5),
+(64, 'Blushing Dreamscape', 'bday', 11500.00, 24, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509311/images/BIRTHDAYFLOWERS/xnksmff0umawglcuqwoz.jpg', 2, 5),
+(65, 'Springtime Fantasy', 'bday', 12000.00, 12, 'https://res.cloudinary.com/dogrgo15f/image/upload/v1725509319/images/BIRTHDAYFLOWERS/vryvirl13wmuilttqjos.jpg', 2, 5);
 
 -- --------------------------------------------------------
 
@@ -151,7 +202,10 @@ INSERT INTO `types` (`id`, `name`, `category_id`) VALUES
 (4, 'Tulip', 1),
 (5, 'Birthday', 2),
 (6, 'Valentine', 2),
-(7, 'Funeral', 2);
+(7, 'Funeral', 2),
+(9, 'wrappers', 3),
+(10, 'ribbons', 3),
+(11, 'flower', 3);
 
 -- --------------------------------------------------------
 
@@ -204,6 +258,13 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -244,19 +305,25 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `types`
 --
 ALTER TABLE `types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -267,6 +334,12 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`ID`);
 
 --
 -- Constraints for table `products`
